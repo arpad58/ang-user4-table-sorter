fb7b14329d494e1b8ec42c957d0c96f50c4167d2
Arpad <solar@hu.inter.net>
2021-03-27 18:01:49 +0100

    table works

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,22 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  /*table */
+  serverAddress: string = 'http://localhost:3000/users'
+
+              /*table                     */
+  constructor(private http: HttpClient) { }
+
+ /*table*/
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.serverAddress}`);
+  }
+}
+
